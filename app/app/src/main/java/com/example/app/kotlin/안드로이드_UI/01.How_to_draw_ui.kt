937690f25984aebfbb65a96d08d9206f1727d601
dustjq1004 <dustjq1005@naver.com>
2021-05-31 23:05:03 +0900

    2021-05-31 android 기초 view01

---
+++ app/app/src/main/java/com/example/app/kotlin/안드로이드_UI/01.How_to_draw_ui.kt
@@ -0,0 +1,18 @@
+package com.example.app.kotlin.안드로이드_UI
+
+// 안드로이드에서 화면을 그리는 방법
+// - XML을 이용한다
+// - DSL Language -> Domain Specific Language
+// - 안드로이드 UI 를 그리기 위해 특화된 언어이다.
+
+// 핸드폰마다 화면 크기가 다른데 어떻게 화면을 그려야 될까?
+// 픽셀, dpi, dp 단위
+// 픽셀 -> 핸드폰 화면에서 빛이 나오는 전구 -> 가장 작은 단위
+// dpi -> dot per inch
+// ldpi -> 120 (1인치에 120 픽셀)
+//       - mdpi, hdpi, xhdpi, xxhdpi
+//
+// dp -> Density Independent Pixcel
+//        픽셀 독립적인 단위
+
+
