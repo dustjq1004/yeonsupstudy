d693a9abd6b310c829408a44efa7efa0f73233fe
dustjq1004 <dustjq1005@naver.com>
2021-06-14 00:45:48 +0900

    안드로이ê드 기초  Library & addView & LIstView & RecyclerView

---
+++ studyApp/app/src/main/java/com/example/studyapp/addViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.studyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_view)
+    }
+}
