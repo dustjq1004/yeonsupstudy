7d4d1a43afa38913e388fcf29e71573ba0b87e03
dustjq1004 <dustjq1005@naver.com>
2021-06-21 23:33:40 +0900

    안드로이드 기초 ì로그인 구현01

---
+++ studyApp/app/src/main/java/com/example/studyapp/EmailSignUpAcitiviy.kt
@@ -0,0 +1,11 @@
+package com.example.studyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailSignUpAcitiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_sign_up_acitiviy)
+    }
+}
